e8f4858bef677f72ccffc9f4fd6a962c5f0fabaf
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2022-04-11 17:00:32 +0200

    do do do stuff

---
+++ browserview/main.js
@@ -0,0 +1,32 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+console.log(process.argv)
+
+function createWindow () {
+    // Create the browser window.
+    const Arguments = process.argv
+    const Size = process.argv[3].split("x")
+    const MainWindow = new BrowserWindow({
+        width: parseInt(Size[0]),
+        height: parseInt(Size[1]),
+        x: parseInt(Size[2]),
+        y: parseInt(Size[3]),
+
+    })
+
+    //if process.argv[2] == "" {
+    MainWindow.loadURL('https://google.com')
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', function () {
+    app.quit()
+})
