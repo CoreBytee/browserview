3fb1921a160b9c395198a1a6457ce78be091125f
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-06-12 13:51:25 +0200

    simple unzip

---
+++ Wrapper/Twr/src/Bootstrap/lua/ga/corebyte/BrowserView/Wrapper/Unzip/Main.lua
@@ -0,0 +1,14 @@
+return function (From, To)
+    local Result = require("coro-spawn")(
+        "tar",
+        {
+            args = {
+                "-xf",
+                From,
+                "-C", To
+            }
+        }
+    )
+    Result.waitExit()
+end
+
