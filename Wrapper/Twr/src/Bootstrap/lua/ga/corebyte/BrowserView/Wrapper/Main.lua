09bfb75048e2a5b7a9bf94c619221bb1a275a56b
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-06-12 13:53:12 +0200

    funcs

---
+++ Wrapper/Twr/src/Bootstrap/lua/ga/corebyte/BrowserView/Wrapper/Main.lua
@@ -0,0 +1,5 @@
+return {
+    Download = Import("ga.corebyte.BrowserView.Wrapper.Downloaders"),
+    LoadAll = Import("ga.corebyte.BrowserView.Wrapper.LoadAll"),
+    NewWindow = Import("ga.corebyte.BrowserView.Wrapper.NewWindow"),
+}
