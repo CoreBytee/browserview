9f5b094b435a9240a5e7dc87123e1e58d2a2f927
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-06-12 14:43:34 +0200

    Update Test.lua

--- Wrapper/Twr/src/Bootstrap/lua/ga/corebyte/BrowserView/Wrapper/Test.lua
+++ Wrapper/Twr/src/Bootstrap/lua/ga/corebyte/BrowserView/Wrapper/Test.lua
@@ -1,4 +1,9 @@
 local Window = Import("ga.corebyte.BrowserView.Wrapper").Download().LoadAll().NewWindow({Stdio = true})
 Window:Start()
 
+Window:OnAny(
+    function (Name, ...)
+        TypeWriter.Logger.Info(Name)
+    end
+)
 Window:LoadURL("http://google.com")
